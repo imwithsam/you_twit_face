7223af3576193d2698cbb99b1634b790d2037592
Samson Brock <SamsonLB@gmail.com>
2015-09-09 00:00:05 -0600

    Add User with Twitter authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+  def create
+    user = User.find_or_create_from_auth(oauth_data)
+
+    if user
+      session[:user_id] = user.id
+      redirect_to dashboard_path
+    else
+      flash[:danger] = "Unable to log in with your Twitter account."
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have been logged out."
+    redirect_to root_path
+  end
+
+  private
+
+  def oauth_data
+    request.env["omniauth.auth"]
+  end
 end
---
+++ spec/features/user_cannot_log_in_with_invalid_twitter_credentials_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.feature "User cannot log in with invalid Twitter credentials", type: :feature do
+  before do
+    # Short circuit OmniAuth requests to use a mock failure
+    OmniAuth.config.test_mode = true
+
+    OmniAuth.config.mock_auth[:twitter] = :invalid_credentials
+
+    Rails.application.env_config["omniauth.auth"] =
+      OmniAuth.config.mock_auth[:twitter]
+  end
+
+  scenario "User tries to log in by clicking Login with Twitter link" do
+    pending "Unsure how to display a flash alert upon auth failure"
+    visit root_path
+    click_link "Login with Twitter"
+
+    within(".alert-danger") do
+      expect(page).to have_content("Unable to log in with your Twitter account.")
+    end
+  end
+end
