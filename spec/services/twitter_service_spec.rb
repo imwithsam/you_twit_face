f3deb77619bd16e615e59fddfd7f7c23b47482d5
Samson Brock <SamsonLB@gmail.com>
2015-09-10 21:45:29 -0600

    Add test for TwitterService user stats

--- spec/services/twitter_service_spec.rb
+++ spec/services/twitter_service_spec.rb
@@ -1,21 +1,39 @@
 require "rails_helper"
 
 RSpec.describe "TwitterService" do
-  # before do
-  #   let(:service) do
-  #     TwitterService.new(user)
-  #   end
-  # end
+  let(:user) do
+    User.new(
+      nickname:     "imwithsam",
+      provider:     "twitter",
+      token:        ENV["twitter_token"],
+      uid:          "12345",
+      image_url:    "https://pbs.twimg.com/profile_images/606479431251550208/uYn3hNom.jpg",
+      name:         "Samson Brock",
+      location:     "Denver, CO",
+      description:  "Turing School student",
+      website_url:  "http://badmotivator.io/",
+      twitter_url:  "http://twitter.com/imwithsam",
+      token_secret: ENV["twitter_token_secret"]
+    )
+  end
 
-  # it "returns user stats" do
-  #   VCR.use_cassette("twiiter_service_spec#user") do
+  let(:service) { TwitterService.new(user) }
 
-  #   end
-  # end
+  it "returns user stats" do
+    VCR.use_cassette("twitter_service_spec#user") do
+      stats = service.user
 
-  # it "returns user tweets" do
-  #   VCR.use_cassette("twiiter_service_spec#tweets") do
+      assert_equal "Samson Brock", stats.name
+      assert_equal "imwithsam", stats.screen_name
+      assert_equal "http://badmotivator.io/", stats.website.to_s
+      assert_equal "Denver, CO", stats.location
+      assert_equal "https://twitter.com/imwithsam", stats.uri.to_s
+    end
+  end
 
-  #   end
-  # end
+  it "returns user tweets" do
+    VCR.use_cassette("twiiter_service_spec#tweets") do
+
+    end
+  end
 end
